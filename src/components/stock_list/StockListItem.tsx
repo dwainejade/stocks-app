f03d5ae55a66d874b7a9a9a2ec87780b92927635
dwainejade <dwainegnd@gmail.com>
2023-07-28 19:28:42 -0400

    setting symbol with list item

--- src/components/stock_list/StockListItem.tsx
+++ src/components/stock_list/StockListItem.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StockContext } from "../../context/StockContext";
 
 interface StockListItemProps {
   symbol: string;
@@ -6,8 +7,13 @@ interface StockListItemProps {
 }
 
 const StockListItem: React.FC<StockListItemProps> = ({ symbol, price }) => {
+  const { setSymbol } = useContext(StockContext);
+
   return (
-    <div className="flex justify-between items-center p-2 h-16 rounded-md hover:bg-gray-500 ">
+    <div
+      className="flex justify-between items-center p-2 h-16 rounded-md hover:bg-gray-500 "
+      onClick={() => setSymbol(symbol)}
+    >
       <span className="font-medium">{symbol}</span>
       <span className="font-light">{price.toFixed(2)}</span>
     </div>
