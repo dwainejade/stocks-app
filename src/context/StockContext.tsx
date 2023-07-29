885494105ac545c80ae5ae1302c4b14336b276d1
dwainejade <dwainegnd@gmail.com>
2023-07-29 18:36:38 -0400

    created favorite stocks for left panel

--- src/context/StockContext.tsx
+++ src/context/StockContext.tsx
@@ -4,10 +4,13 @@ import { getUnixTimestamps } from "../utils/helpers";
 import localforage from "localforage";
 
 export const StockContext = React.createContext<StocksContextType | null>(null);
+const KEY = import.meta.env.VITE_APP_API_KEY;
 
 const StockProvider = ({ children }) => {
   const [stock, setStock] = React.useState<StockItem | null>(null);
   const [symbol, setSymbol] = React.useState<string>("AAPL");
+  const [favoriteStocks, setFavoriteStocks] = React.useState<StockItem[]>([]);
+  const favoriteSymbols = ["MSFT", "AAPL", "NFLX", "WMT", "META"];
 
   //   const getStock = async (name: string) => {
   //     try {
@@ -26,28 +29,39 @@ const StockProvider = ({ children }) => {
   //     }
   //   };
 
+  const getFavoriteStocks = async () => {
+    const stocks = [];
+    for (const symbol of favoriteSymbols) {
+      const response = await fetch(
+        `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${KEY}`
+      );
+      const quote = await response.json();
+      stocks.push({ symbol, quote });
+    }
+    setFavoriteStocks(stocks);
+  };
+
   const getStock = async () => {
     try {
       // Check if data in localforage is less than a minute old
       const storedStock = await localforage.getItem<StockItem>(symbol);
       if (storedStock && Date.now() - storedStock.lastUpdated < 60 * 1000) {
         setStock(storedStock);
+        console.log("From localForage");
         return;
+      } else {
+        console.log("Fetching from API");
       }
 
       // Fetch data from API
       const candlesResponse = await fetch(
         `https://finnhub.io/api/v1/stock/candle?symbol=${symbol}&resolution=D&from=${
           getUnixTimestamps(new Date()).oneYearAgo
-        }&to=${getUnixTimestamps(new Date()).today}&token=${
-          import.meta.env.VITE_APP_API_KEY
-        }`
+        }&to=${getUnixTimestamps(new Date()).today}&token=${KEY}`
       );
 
       const quoteResponse = await fetch(
-        `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${
-          import.meta.env.VITE_APP_API_KEY
-        }`
+        `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${KEY}`
       );
 
       if (!quoteResponse.ok || !candlesResponse.ok) {
@@ -81,8 +95,21 @@ const StockProvider = ({ children }) => {
     getStock();
   }, [symbol]);
 
+  React.useEffect(() => {
+    getFavoriteStocks();
+  }, []);
+
   return (
-    <StockContext.Provider value={{ stock, getStock, symbol, setSymbol }}>
+    <StockContext.Provider
+      value={{
+        stock,
+        getStock,
+        symbol,
+        setSymbol,
+        favoriteStocks,
+        setFavoriteStocks,
+      }}
+    >
       {children}
     </StockContext.Provider>
   );
