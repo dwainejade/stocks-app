ca66939f28a1cb9b55fc617a6dec6acfe33e2146
dwainejade <dwainegnd@gmail.com>
2023-07-29 09:09:51 -0400

    handle stock onclick

--- src/components/stock_list/StockListItem.tsx
+++ src/components/stock_list/StockListItem.tsx
@@ -8,11 +8,14 @@ interface StockListItemProps {
 
 const StockListItem: React.FC<StockListItemProps> = ({ symbol, price }) => {
   const { setSymbol } = useContext(StockContext);
+  const handleClick = () => {
+    setSymbol(symbol);
+  };
 
   return (
     <div
       className="flex justify-between items-center p-2 h-16 rounded-md hover:bg-gray-500 "
-      onClick={() => setSymbol(symbol)}
+      onClick={handleClick}
     >
       <span className="font-medium">{symbol}</span>
       <span className="font-light">{price.toFixed(2)}</span>
