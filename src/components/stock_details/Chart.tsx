1e6bac785d39aedda7522e6f01396a834799b688
dwainejade <dwainegnd@gmail.com>
2023-07-30 02:00:58 -0400

    chart styling

--- src/components/stock_details/Chart.tsx
+++ src/components/stock_details/Chart.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import {
   Area,
   AreaChart,
@@ -33,26 +33,29 @@ const Chart = () => {
   }));
 
   return (
-    <ResponsiveContainer width={windowWidth - 450} height={250}>
+    <ResponsiveContainer
+      width={windowWidth - 450 > 800 ? 800 : windowWidth - 450}
+      height={250}
+    >
       <AreaChart
         data={transformedPrices}
         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
       >
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={1} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0.2} />
           </linearGradient>
         </defs>
         <XAxis dataKey="date" />
         <YAxis />
-        {/* <CartesianGrid strokeDasharray="3 3" /> */}
+        {/* <CartesianGrid strokeDasharray="3 3" strokeOpacity={0.1} /> */}
         <Tooltip />
         <Area
           type="monotone"
           dataKey="price"
           stroke="#8884d8"
-          fillOpacity={1}
+          fillOpacity={0.2}
           fill="url(#colorUv)"
         />
       </AreaChart>
