04d42b3494561e423de97ea965de1fd8292ca8bf
dwainejade <dwainegnd@gmail.com>
2023-07-30 01:30:14 -0400

    text styling

--- src/components/stock_details/StockInfo.tsx
+++ src/components/stock_details/StockInfo.tsx
@@ -1,64 +1,56 @@
-import React from 'react';
-import { mockListData } from '../../constants/mockData';
-
-interface StockInfoProps {
-  symbol: string;
-  quote: {
-    c: number;
-    d: number;
-    dp: number;
-    h: number;
-    l: number;
-    o: number;
-    pc: number;
-    t: number;
-  };
-}
-
-const sampleStock: StockInfoProps = mockListData[1];
-console.log(sampleStock);
+import React, { useContext } from "react";
+import { StockContext } from "../../context/StockContext";
+import { StocksContextType } from "../../utils/interfaces";
 
 const StockInfo: React.FC = () => {
+  const { stock } = useContext(StockContext) as StocksContextType;
+
+  if (!stock) return null;
+
+  const conStyle = "flex flex-1 justify-between px-2 py-1";
+  const leftTextStyle = "text-gray-400";
+  const rightTextStyle = "text-white";
+
   return (
-    <div className="grid grid-cols-3 divide-x-2">
+    <div className="grid grid-cols-3 divide-x-2 divide-gray-500">
       <div>
-        <div className="flex flex-1 justify-between px-2 py-1">
-          <p>c</p>
-          <p>{sampleStock.quote.c}</p>
+        <div className={conStyle}>
+          <p className={leftTextStyle}>Close</p>
+          <p className={rightTextStyle}>{stock.quote.c}</p>
         </div>
-        <div className="flex flex-1 justify-between px-2 py-1">
-          <p>d</p>
-          <p>{sampleStock.quote.d}</p>
+        <div className={conStyle}>
+          <p className={leftTextStyle}>Day</p>
+          <p className={rightTextStyle}>{stock.quote.d}</p>
         </div>
-        <div className="flex flex-1 justify-between px-2 py-1">
-          <p>dp</p>
-          <p>{sampleStock.quote.dp}</p>
+        <div className={conStyle}>
+          <p className={leftTextStyle}>Day Percentage</p>
+          <p className={rightTextStyle}>{stock.quote.dp}</p>
         </div>
       </div>
 
       <div>
-        <div className="flex flex-1 justify-between px-2 py-1">
-          <p>h</p>
-          <p>{sampleStock.quote.h}</p>
+        <div className={conStyle}>
+          <p className={leftTextStyle}>High</p>
+          <p className={rightTextStyle}>{stock.quote.h}</p>
         </div>
-        <div className="flex flex-1 justify-between px-2 py-1">
-          <p>l</p>
-          <p>{sampleStock.quote.l}</p>
+        <div className={conStyle}>
+          <p className={leftTextStyle}>Low</p>
+          <p className={rightTextStyle}>{stock.quote.l}</p>
         </div>
-        <div className="flex flex-1 justify-between px-2 py-1">
-          <p>o</p>
-          <p>{sampleStock.quote.o}</p>
+        <div className={conStyle}>
+          <p className={leftTextStyle}>Open</p>
+          <p className={rightTextStyle}>{stock.quote.o}</p>
         </div>
       </div>
 
       <div>
-        <div className="flex flex-1 justify-between px-2 py-1">
-          <p>pc</p>
-          <p>{sampleStock.quote.pc}</p>
+        <div className={conStyle}>
+          <p className={leftTextStyle}>Previous Close</p>
+          <p className={rightTextStyle}>{stock.quote.pc}</p>
         </div>
-        <div className="flex flex-1 justify-between px-2 py-1">
-          <p>t</p>
-          <p>{sampleStock.quote.t}</p>
+        <div className={conStyle}>
+          <p className={leftTextStyle}>Time</p>
+          <p className={rightTextStyle}>{stock.quote.t}</p>
         </div>
       </div>
     </div>
