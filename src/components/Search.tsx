c3bd33534fd10928280c0e9de7d3d6b08bb6850b
dwainejade <dwainegnd@gmail.com>
2023-07-31 18:07:01 -0400

    search working

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,6 +3,7 @@ import { StockContext } from "../context/StockContext";
 import { MagnifyingGlassIcon, XCircleIcon } from "@heroicons/react/24/solid";
 
 interface SearchResult {
+  symbol: string;
   description: string;
 }
 
@@ -23,12 +24,21 @@ const StockSearch = () => {
     setError(null);
     try {
       const results = await searchStock(inputValue);
-      setSearchResults(results.result);
+      const filteredResults = results.result.filter(
+        (result) => result.type === "Common Stock"
+      );
+      setSearchResults(filteredResults);
     } catch (err) {
       setError(err.message);
     } finally {
       setIsLoading(false);
     }
+  };
+
+  const handleSelectedResult = (symbol: string) => {
+    setSymbol(symbol);
+    setInputValue("");
+    setSearchResults([]);
   };
 
   console.log(searchResults);
@@ -55,27 +65,30 @@ const StockSearch = () => {
             className="absolute right-0 pr-2 text-gray-400 cursor-pointer"
             onClick={() => setInputValue("")}
           >
-            <XCircleIcon className="h-6 w-6 text-gray-400 " />
+            <XCircleIcon className="h-6 w-6 text-gray-400" />
           </span>
         )}
       </form>
-
-      <div className="absolute w-full mt-2 bg-gray-400 rounded shadow-lg">
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : error ? (
-          <p>Error: {error}</p>
-        ) : (
-          searchResults?.map((result, index) => (
-            <div
-              key={index}
-              className={`px-4 py-2 border-b border-gray-700 last:border-b-0`}
-            >
-              {result.description}
-            </div>
-          ))
-        )}
-      </div>
+      {inputValue && searchResults && (
+        <div className="absolute w-full max-h-[500px] mt-3 rounded-md shadow-lg overflow-y-auto bg-gray-900 px-2 hover:cursor-pointer">
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>Error: {error}</p>
+          ) : (
+            searchResults?.map((result, index) => (
+              <div
+                key={index}
+                className="px-4 py-2 border-b border-gray-800 last:border-0"
+                onClick={() => handleSelectedResult(result.symbol)}
+              >
+                <p className="font-bold">{result.symbol}</p>
+                <p className="text-gray-500 text-xs">{result.description}</p>
+              </div>
+            ))
+          )}
+        </div>
+      )}
     </div>
   );
 };
