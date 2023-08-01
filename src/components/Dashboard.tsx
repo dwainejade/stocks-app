911b38f17b6a86fe0d3d64f098fad033d724aa49
dwainejade <dwainegnd@gmail.com>
2023-08-01 18:03:54 -0400

    fixed overflow

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -7,7 +7,7 @@ import StockInfo from "./stock_details/StockInfo";
 
 const StockDashboard: React.FC = () => {
   return (
-    <div className="flex h-screen w-screen bg-gray-800 text-gray-50">
+    <div className="flex h-screen w-screen bg-gray-800 text-gray-50 overflow-none">
       {/* Left side with list of stocks */}
       <div className="w-[330px] flex-shrink-0 h-full flex flex-col bg-grey-100">
         <div className="border-b-2 border-gray-600 p-3 bg-gray-800">
@@ -19,7 +19,7 @@ const StockDashboard: React.FC = () => {
       </div>
 
       {/* Right side with chart and stock details */}
-      <div className="flex-grow flex justify-center items-start bg-gray-900 text-gray-50 p-5 ">
+      <div className="flex-grow flex justify-center items-start bg-gray-900 text-gray-50 p-5 overflow-y-auto">
         <div className="max-w-[900px] flex flex-col items-center justify-center">
           <DetailsHeader />
           <Chart />
