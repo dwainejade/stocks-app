6ff3062bc46e6056f343ebcbcce1297b1949018f
dwainejade <dwainegnd@gmail.com>
2023-08-02 17:03:15 -0400

    fixed company names

--- src/components/stock_details/DetailsHeader.tsx
+++ src/components/stock_details/DetailsHeader.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { StockContext } from "../../context/StockContext";
 import { StocksContextType } from "../../utils/interfaces";
 import { CheckCircleIcon, PlusIcon } from "@heroicons/react/24/solid";
+import companyStocks from "../../constants/companySymbols.json";
 
 const DetailsHeader: React.FC = () => {
   const { stock, favoriteStocks, setFavoriteStocks } = useContext(
@@ -26,6 +27,18 @@ const DetailsHeader: React.FC = () => {
     }
   };
 
+  // Find the company name from the companyStocks JSON
+  const companyName = companyStocks.find(
+    (company) => company.symbol === stock.symbol
+  )?.description;
+
+  const capitalize = (str) => {
+    return str
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(" ");
+  };
+
   return (
     <>
       <div className="w-full flex justify-end mb-2">
@@ -45,8 +58,10 @@ const DetailsHeader: React.FC = () => {
       </div>
       <div className="w-full h-30 flex flex-row justify-between items-center pb-4 border-b border-gray-600">
         <div className="flex flex-col">
-          <span className="text-4xl">{stock?.symbol}</span>
-          <span className="text-md">Microsft Corporation</span>
+          <span className="text-4xl font-bold">{stock.symbol}</span>
+          <span className="text-md text-gray-400">
+            {companyName && capitalize(companyName)}
+          </span>
         </div>
         <div className="flex flex-col">
           <span>${stock?.quote.c.toFixed(2)}</span>
@@ -55,8 +70,8 @@ const DetailsHeader: React.FC = () => {
               stock.quote.d > 0 ? "text-green-500" : "text-red-500"
             }`}
           >
-            {stock?.quote.d > 0 ? "+" : ""}
-            {stock?.quote.d.toFixed(2)}
+            {stock.quote.d > 0 ? "+" : ""}
+            {stock.quote.d.toFixed(2)}
           </span>
         </div>
       </div>
