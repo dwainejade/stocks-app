45c5b01aae5aafd7c0ba271419609dce93c1e0f2
dwainejade <dwainegnd@gmail.com>
2023-08-05 17:27:33 -0400

    stagger animation for news cards

--- src/components/news/NewsList.tsx
+++ src/components/news/NewsList.tsx
@@ -1,17 +1,18 @@
 import { useContext, useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 import { StockContext } from "../../context/StockContext";
 import { StocksContextType, NewsItem } from "../../utils/interfaces";
 import NewsCard from "./NewsCard";
 
 const NewsList = () => {
   const { news } = useContext(StockContext) as StocksContextType;
-  const [loadMore, setLoadMore] = useState(6); // Load 10 news items at a time
+  const [loadMore, setLoadMore] = useState(6);
   const loader = useRef<HTMLDivElement | null>(null);
 
   const handleObserver = (entities: IntersectionObserverEntry[]) => {
     const target = entities[0];
     if (target.isIntersecting) {
-      setLoadMore((prev) => prev + 6);
+      setLoadMore((prev) => prev + 6); // Load 6 news items at a time
     }
   };
 
@@ -27,13 +28,28 @@ const NewsList = () => {
     }
   }, []);
 
+  const variants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 xlg:grid-cols-3 gap-3">
+    <motion.div
+      className="grid grid-cols-1 lg:grid-cols-2 xlg:grid-cols-3 gap-3"
+      variants={variants}
+      initial="hidden"
+      animate="show"
+    >
       {news.slice(0, loadMore).map((newsItem: NewsItem, index: number) => (
         <NewsCard key={index} newsItem={newsItem} />
       ))}
       <div ref={loader} />
-    </div>
+    </motion.div>
   );
 };
 
